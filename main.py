afca2f8d349166c3b0643be021071ca3dd6f8edf
JavierRant <92230003+JavierRant@users.noreply.github.com>
2023-09-03 18:46:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,88 +1,71 @@
-import cv2
+# importar bibliotecas
+import cv2 as cv
+import numpy as np
+import matplotlib
+from scipy import ndimage
+from skimage import io, color, measure
 
-from contour import get_contours, get_biggest_contour, compare_contours
-from frame_editor import apply_color_convertion, adaptive_threshold, denoise, draw_contours
+# definicion de variables
+maxValue = 100
+minValue = 0
+windowName1 = 'levadura'
+windowName2 = 'mapa de color'
+trackbarName = 'umbral'
 
+img1 = cv.imread('../static/images/levadura.png')
+img = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
 
-def on_trackbar(val):
-    pass
+# creación de la función recursiva que llama la funcion createTrackbar()
+def segementation(trackbarValue):
+    _, thresh = cv.threshold(img, trackbarValue, maxValue, cv.THRESH_BINARY)
 
-def main():
+    # Noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=7)
 
-    # definción devariables
-    max_value = 255
-    threshold_trackbar_name = "threshold"
-    denoise_trackbar_name = "denoise"
-    window_name = 'Window'
-    color_green = (0, 255, 0)
+    sure_bg = cv.dilate(opening, kernel, iterations=2) # Esto es background (sure background)
 
-    # creación de la ventana
-    cv2.namedWindow(window_name)
+    dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 3)
+    _, sure_fg = cv.threshold(dist_transform, 0.01*dist_transform.max(), 255, 0)
+    sure_fg = np.uint8(sure_fg) # Estas son celulas (sure foreground)
 
-    # creación de la trackbar para el threshold y el denoise
-    cv2.createTrackbar(threshold_trackbar_name, window_name, 0, max_value, on_trackbar)
-    cv2.createTrackbar(denoise_trackbar_name, window_name, 0, max_value, on_trackbar)
+    unknown = cv.subtract(sure_bg, sure_fg) # pixeles q no sabemos q son exactamente
+    
+    # individualizacion de las celulas
+    num, markers = cv.connectedComponents(sure_fg)
+    # aprovechamos el el hecho de que la funcion connectedComponentes() nos
+    # devuelva la cantidad de etiquetas para coontar las celulas
+    strNUM = str(num-1) # para que no cuente el fondo
+    cantCelulas = 'cant celulas:'+ strNUM
 
-    # captura el video de la WebCam
-    webcam = cv2.VideoCapture(0)
+    markers = markers+1
 
-    # como VideoCapture devuelve un boolean me puedo asegurar de que este capturando correctamente
-    while True:
-        _, frame = webcam.read() # repetir para refrescar la imagen
+    markers[unknown == 255] = 0
 
-        # paso a formato monocromatico
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    markers = cv.watershed(img1, markers)
 
-        # consigo el valor des las trackbar
-        threshold_value = cv2.getTrackbarPos(threshold_trackbar_name , window_name)
-        denoise_value = cv2.getTrackbarPos(denoise_trackbar_name , window_name)
+    img1[markers == -1] = [0, 255, 255]
 
-        # paso a formato binario
-        _, tresh = cv2.threshold(gray, threshold_value, max_value, cv2.THRESH_BINARY )
+    labelIMG = np.uint8(255 * markers / num) # convertir los labels en mapa de semillas
 
-        # saco el ruido
-        noiseless = cv2.fastNlMeansDenoising(tresh, None, denoise_value, 7, 21)
-        
-        contours = get_contours(noiseless, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        
-        for contour in contours:
-            area = cv2.contourArea(contour)
+    colorMap = cv.applyColorMap(labelIMG, cv.COLORMAP_JET) # crea el mapa de marcadores
 
-            if area > 2000:
-                draw_contours(frame, contour, color_green, 3)
-                approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
-            
-                x = approx.ravel() [0]
-                y = approx.ravel() [1]
+    cv.putText(thresh, cantCelulas, (30, 700), cv.FONT_HERSHEY_SIMPLEX , 1, 255)  # muestra la imagen actualizada con los valores de threshold
 
-                if len(approx) == 3:
-                    cv2.putText(frame, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, color_green)
-                elif len(approx) == 4:
-                    x, y, w, h = cv2.boundingRect(approx)
-                    aspectRatio = float(w)/h
-                    if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-                        cv2.putText(frame, "Square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, color_green)
-                    else:
-                        cv2.putText(frame, "Rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, color_green)
-                elif len(approx) == 5:
-                    cv2.putText(frame, "Pentagon", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, color_green)
-                elif len(approx) == 10:
-                    cv2.putText(frame, "Star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, color_green)
-                else:
-                    cv2.putText(frame, "Circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, color_green)
+    clusters = measure.regionprops(label_image=labelIMG, intensity_image=img1) # para medir el area
 
+    cv.imshow(windowName1, thresh)
+    cv.imshow(windowName2, colorMap)
+    # cv.imshow("Opening", opening)
+    # cv.imshow("Sure background", sure_bg)
+    # cv.imshow("Sure foreground", sure_fg)
+    # cv.imshow("Unkown", unknown)
 
-        # muestro la imagen
-        cv2.imshow('color', frame)
-        cv2.imshow(window_name, noiseless)
+# crea el trackbar y lo asocia a la imagen
+cv.namedWindow(windowName1)
 
+cv.createTrackbar(trackbarName, windowName1, minValue, maxValue, segementation)
 
+segementation(0)
 
-
-        tecla = cv2.waitKey(30)  # espera 30 ms para que se presione una tecla . El mínimo es 1 ms.  tecla == 0 si no se pulsó ninguna.
-        if tecla == 27:	# tecla ESC para salir
-            break
-
-
-
-main()
+cv.waitKey(0)
